3a68d24cda3a584580ec67c7c980059ee73f4cec
Fabrizzio Aranda <fabrizzio1996@gmail.com>
2021-01-31 21:19:37 -0500

    first ccommit

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const app = express();
-
+const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let counter = 0;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const {Schema} = mongoose;
+const urlSchema = new Schema({
+    url: String,
+    shortUrl: String
+})
+
+const UrlModel = mongoose.model('Url', urlSchema);
+
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +31,37 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl/new', (req, res) => {
+  const {url} = req.body;
+  dns.lookup(url, (err, address, family) => {
+    if(err) {
+      res.json({"error": "invalid url"});
+    } else {
+      const original_url = `http://${url}`;
+      const short_url = ++counter;
+      let variable = new UrlModel({"url": original_url, "shortUrl": short_url});
+      variable.save((err, result) => {
+        if(err) {
+          res.status(404).json({"error": err});
+      } else {
+          res.send("Record saved")
+      }
+    })
+    } 
+  })
+})
+
+app.get('/api/shorturl/:url', (req, res) => {
+  const {url} = req.params;
+  UrlModel.find({"shortUrl": url}, (err, doc) => {
+    if(err) {
+      res.json({"error": "Invalid error"})
+    } else {
+      res.redirect(doc[0].url);
+    }
+  })
 });
 
 app.listen(port, function() {
