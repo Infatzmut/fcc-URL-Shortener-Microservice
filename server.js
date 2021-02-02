c7c8b084bcdcd533447132e4bbe0d882a369fb68
Fabrizzio Aranda <fabrizzio1996@gmail.com>
2021-02-01 20:13:11 -0500

    new push

--- server.js
+++ server.js
@@ -8,17 +8,7 @@ const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 let counter = 0;
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-.catch(error => console.log(error));
-
-const {Schema} = mongoose;
-const urlSchema = new Schema({
-    url: String,
-    shortUrl: String
-})
-
-const UrlModel = mongoose.model('Url', urlSchema);
-
+let savedData = []
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -36,35 +26,32 @@ app.get('/api/hello', function(req, res) {
 
 app.post('/api/shorturl/new', (req, res) => {
   const {url} = req.body;
-  dns.lookup(url, (err, address, family) => {
-    if(err) {
-      console.error(err);
-      res.json({"error": "invalid url"});
-    } else {
-      counter++;
-      const original_url = `http://${url}`;
-      const short_url = counter;
-      let variable = new UrlModel({"url": original_url, "shortUrl": short_url});
-      variable.save((err, result) => {
-        if(err) {
-          res.status(404).json({"error": err});
+  if(!url) {
+    res.json({"error":"Invalid url"})
+  } else {
+    dns.lookup(url, (err, address, family) => {
+      if(err) {
+        console.error(err);
+        res.json({"error": "invalid url"});
       } else {
-          res.send("Record saved")
-      }
+        counter++;
+        const original_url = `http://${url}`;
+        const short_url = counter;
+        let variable = {"url": original_url, "shortUrl": short_url};
+        savedData.push(variable);
+        res.json({"msg": "saved"});
+      } 
     })
-    } 
-  })
+  }
 })
 
 app.get('/api/shorturl/:url', (req, res) => {
   const {url} = req.params;
-  UrlModel.find({"shortUrl": url}, (err, doc) => {
-    if(err) {
-      res.json({"error": err})
-    } else {
-      res.redirect(doc[0].url);
-    }
-  })
+  console.info("urls guardadas: ");
+  console.info(savedData);
+  let doc = savedData.filter(data => data.shortUrl == url)
+  console.log(doc);
+  res.redirect(doc[0].url);
 });
 
 app.listen(port, function() {
