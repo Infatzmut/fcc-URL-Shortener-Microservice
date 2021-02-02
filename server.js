b7fec12bf00e2eb3f67950913e8832bb964ead5f
Fabrizzio Aranda <fabrizzio1996@gmail.com>
2021-02-01 21:51:54 -0500

    hope

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const app = express();
 const validUrl = require('valid-url');
+const shortId = require('shortid');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 let counter = 0;
 let savedData = []
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error'));
+connection.once('open', () => {
+  console.log("Connected to mongo db");
+})
+
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url: String, 
+  short_url: String
+});
+const URL = mongoose.model('URL', urlSchema);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,34 +40,52 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl/new', (req, res) => {
-  const {url} = req.body;
-  if(!url) {
-    res.json({"error":"Invalid url"})
+app.post('/api/shorturl/new', async (req, res) => {
+  const url = req.body.url;
+  const urlCode = shortId.generate();
+  if(!validUrl.isWebUri(url)) {
+    res.json({"error": "invalid url"})
   } else {
-    if(!validUrl.isWebUri(url)) {
-      res.status(401).json({"error":"invalid URL"});
-    } else {
-      counter++;
-      const short_url = counter;
-      let variable = {"original_url": url, "shortUrl": short_url};
-      savedData.push(variable);
-      res.json({"msg": "saved"});
-    } 
+    try {
+      let findOne = await URL.findOne({
+        original_url: url
+      })
+      if(findOne) {
+        res.json({
+          "original_url": findOne.original_url,
+          "short_url": findOne.short_url
+        })
+      } else {
+        findOne = new URL({
+          original_url: url,
+          short_url: urlCode
+        })
+        await findOne.save();
+        res.json({
+          "original_url": findOne.original_url,
+          "short_url": findOne.short_url
+        })
+      }
+    } catch(error) {
+      console.error(error);
+      res.status(500).json({"msg":"Server error"})
+    }
   }
 })
 
-app.get('/api/shorturl/:url?', (req, res) => {
-  console.log(req.params)
-  if(!req.params.url || req.params.url == 'undefined') {
-    res.json({"msg": "Invalid url"});
-  } else {
-    const {url} = req.params;
-    console.info("urls guardadas: ");
-    console.info(savedData);
-    let doc = savedData.filter(data => data.shortUrl == url)
-    console.log(doc);
-    res.redirect(doc[0].url);
+app.get('/api/shorturl/:url?', async (req, res) => {
+  try {
+    const urlParams = await URL.findOne({
+      short_url: req.params.url
+    })
+    if(urlParams) {
+      return res.redirect(urlParams.original_url)
+    } else {
+      return res.status(404).json({"error": "invalid url"});
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({"msg": err});
   }
 });
 
